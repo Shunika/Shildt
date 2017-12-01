674736569cc3d3a4df1620b48acc6dadfbf24e20
Shunika <nastashu@gmail.com>
2017-12-01 23:32:49 +0300

    Добавлены комментарияя к классу Example

--- src/Example.java
+++ src/Example.java
@@ -1,8 +1,8 @@
 /**
  * Created by shunya on 01.12.2017.
  */
-public class Example {
-  public static void main (String args[]){
-    System.out.println("Java drives the Web");
+public class Example {                             //public, поскольку при выполнении программы он вызывается за пределами своего класса.
+  public static void main (String args[]){         //static допускает вызов метода main () до создания объекта класса
+    System.out.println("Java drives the Web");     // void лишь сообщает компилятору о том, что метод main () не возвращает значение.
   }
 }
