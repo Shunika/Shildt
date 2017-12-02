86eaa4298b433dab8fc417b5f4f2630dac98911e
Shunika <nastashu@gmail.com>
2017-12-02 23:38:08 +0300

    Добавлен класс преобразования галлонов в литры

---
+++ src/GalToLit.java
@@ -0,0 +1,16 @@
+/**
+ * Created by shunya on 02.12.2017.
+ */
+//Преобразование галлонов в литры
+public class GalToLit {
+  public static void main (String args []){
+  double gallon; //	в этой переменной хранится объем жидкости	в	галлонах
+  double liters;  //	в этой переменной хранится	объем жидкости	в	литрах
+
+    gallon =10;
+    liters = gallon *3.7854;
+    System.out.println(gallon + " gallons is " + liters + " liters.");
+
+
+  }
+}
